8ad8640152aecd75170175359b22ea4ed8d8a98f
Huangxy-Minel <huangxy_minel@foxmail.com>
2024-09-15 13:17:51 +0800

    minors

---
+++ third_party/build_dpdk.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+CUR_DIR=$(cd `dirname $0`; pwd)
+cd $CUR_DIR
+# if the build folder exists, remove it
+if [ -d "build" ]; then
+    rm -rf build
+fi
+cd dpdk-stable-22.11.3
+meson setup -Dexamples=all -Denable_kmods=false -Dtests=false -Ddisable_drivers='raw/*,crypto/*,baseband/*,dma/*' build
+meson configure -Dprefix=${CUR_DIR}/dpdk-stable-22.11.3/build/install build
+ninja -C build install
