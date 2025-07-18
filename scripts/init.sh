be48235a8dcc4295b279d128b6a304bb07c70287
Huangxy-Minel <huangxy_minel@foxmail.com>
2025-07-18 12:38:45 +0000

    [Bugs]: minor

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,7 +2,7 @@
 CUR_DIR=$(cd `dirname $0`; pwd)
 cd $CUR_DIR
 # basic dependencies
-sudo apt-get install make build-essential gcc g++ libudev-dev libnl-3-dev libnl-route-3-dev ninja-build pkg-config valgrind python3-dev cython3 python3-docutils pandoc
+sudo apt-get install make build-essential gcc g++ libudev-dev libnl-3-dev libnl-route-3-dev ninja-build pkg-config valgrind python3-dev cython3 python3-docutils pandoc meson
 # Axio dependencies
 sudo apt install libnuma-dev libgflags-dev numactl libsystemd-dev libidn11-dev
 # perf dependencies
