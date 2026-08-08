98413a0f92a4f4215f409bff27e9ee52d0a90990
Huangxy-Minel <huangxy_minel@foxmail.com>
2026-08-09 03:38:14 +0800

    build: declare datapath sources directly in meson

--- scan_src.py
+++
@@ -1,9 +0,0 @@
-import sys
-import glob
-
-# add all local source files
-sources = glob.glob("./src/**/*.c", recursive=True) + glob.glob("./src/**/*.cpp", recursive=True) + glob.glob("./src/**/*.cc", recursive=True)
-    
-for i in sources:
-    print(i)
-    
